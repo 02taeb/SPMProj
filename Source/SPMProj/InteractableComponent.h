272b7f1da9c87a85ed73e64251a039080e0c7110
Lucas Ilstedt <lucasilstedt@gmail.com>
2023-04-07 13:38:35 +0200

    Making Swinging Door Blueprint

---
+++ Source/SPMProj/InteractableComponent.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "InteractableComponent.generated.h"
+
+
+UCLASS( Blueprintable, ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class SPMPROJ_API UInteractableComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UInteractableComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent)
+	void Interact(AActor* Instigator);
+	
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+};
