b78bf92ba924157014416ff677481c06db4c2258
Daniel Olsson <125807379+OfficialBabyboidaniel@users.noreply.github.com>
2023-05-01 10:21:09 +0200

    Import av animationer
    Lite ändring av AI controller

--- Source/SPMProj/AI_EnemyController.cpp
+++ Source/SPMProj/AI_EnemyController.cpp
@@ -6,11 +6,12 @@
 #include "PlayerCharacter.h"
 #include "Perception/AISenseConfig_Sight.h"
 
+
 AAI_EnemyController::AAI_EnemyController(const FObjectInitializer& ObjectInitializer)
 {
 	AIPerceptionComponent = CreateDefaultSubobject<UAIPerceptionComponent>(TEXT("AIPerceptionComponent"));
 
-	// AIPerceptionComponent->bAutoActivate = true;
+	AIPerceptionComponent->bAutoActivate = true;
 
 	SetPerceptionComponent(*AIPerceptionComponent);
 
@@ -18,37 +19,26 @@ AAI_EnemyController::AAI_EnemyController(const FObjectInitializer& ObjectInitial
 	Sight = CreateDefaultSubobject<UAISenseConfig_Sight>(TEXT("Sight"));
 
 	// sight variables
-	Sight->SightRadius = 500.0f;
-	Sight->LoseSightRadius = 600.0f;
-	Sight->PeripheralVisionAngleDegrees = 90.0f;
-	Sight->DetectionByAffiliation.bDetectEnemies = true;
-	Sight->DetectionByAffiliation.bDetectNeutrals = true;
-	Sight->DetectionByAffiliation.bDetectFriendlies = false;
+	Sight->SightRadius = SightRadius;
+	Sight->LoseSightRadius = LoseSightRadius;
+	Sight->PeripheralVisionAngleDegrees = PeripheralVisionAngleDegrees;
+	Sight->DetectionByAffiliation.bDetectEnemies = DetectEnemies;
+	Sight->DetectionByAffiliation.bDetectNeutrals = DetectNeutrals;
+	Sight->DetectionByAffiliation.bDetectFriendlies = DetectFriendies;
 
 	// configure sense
 	AIPerceptionComponent->ConfigureSense(*Sight);
+	
 	// set dominant sense
 	AIPerceptionComponent->SetDominantSense(Sight->GetSenseImplementation());
 	
 	UE_LOG(LogTemp, Display, TEXT("Creating Enemy"));
-	if (GetPawn() != nullptr)
-	{
-		UE_LOG(LogTemp, Display, TEXT("setting start location"));
-		GetBlackboardComponent()->SetValueAsVector(TEXT("StartLocation"), GetPawn()->GetActorLocation());
-	}	
 }
 
 void AAI_EnemyController::BeginPlay()
 {
 	Super::BeginPlay();
-	if (AI_EnemyBehavior != nullptr)
-	{
-		RunBehaviorTree(AI_EnemyBehavior);
-
-		//varför skrev du denna kod daniel lmao
-		//if (const APawn *PlayerPawn = UGameplayStatics::GetPlayerPawn(GetWorld(), 0); PlayerPawn == nullptr)
-		//  return;
-	}
+	
 }
 
 void AAI_EnemyController::OnPossess(APawn* InPawn)
@@ -56,17 +46,21 @@ void AAI_EnemyController::OnPossess(APawn* InPawn)
 	Super::OnPossess(InPawn);
 
 	UE_LOG(LogTemp, Display, TEXT("OnPossess method being called"));
+	
 	AIPerceptionComponent->OnTargetPerceptionUpdated.AddDynamic(this, &AAI_EnemyController::OnPerception);
 }
 
 void AAI_EnemyController::OnPerception(AActor* Actor, FAIStimulus Stimulus)
 {
 	UE_LOG(LogTemp, Display, TEXT("IN onperception method"));
-	if (const APlayerCharacter* Player = Cast<APlayerCharacter>(Actor); Player == nullptr)
+	APlayerCharacter* Player = Cast<APlayerCharacter>(Actor);
+	if ( Player == nullptr)
 		return;
 
 	UE_LOG(LogTemp, Display, TEXT("Onperception is calling"));
 	// Agent->SetAnimState(Stimulus.WasSuccessfullySensed());
+
+	// SetFocus(Stimulus.WasSuccessfullySensed() ? Player : nullptr);
 
 	if (Stimulus.WasSuccessfullySensed())
 	{
