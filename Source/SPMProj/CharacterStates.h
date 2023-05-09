b788a7677be593f5166c6d4bdff572919b21c2d2
Lucas Ilstedt <lucasilstedt@gmail.com>
2023-05-09 12:52:09 +0200

    Eating corpses

--- Source/SPMProj/CharacterStates.h
+++ Source/SPMProj/CharacterStates.h
@@ -13,5 +13,6 @@ enum class ECharacterActionState : uint8
 	ECAS_NoAction UMETA(DisplayName = "NoAction"),
 	ECAS_AttackingNormal UMETA(DisplayName = "AttackingNormal"),
 	ECAS_AttackingHeavy UMETA(DisplayName = "AttackingHeavy"),
-	ECAS_Dodging UMETA(DisplayName = "Dodging")
+	ECAS_Dodging UMETA(DisplayName = "Dodging"),
+	ECAS_Crouching UMETA(DisplayName = "Crouching")
 };
